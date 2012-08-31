86059c8ae5a948059b454516b5daa6f7e1043f52
Wayne Graham <wayne.graham@virginia.edu>
2012-08-31 10:32:25 -0400

    initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwinburneOnRails::Application.config.secret_token = '95f7f9d343c4ba48ca0ae0eecf66ed44eba2934d84f918b757128915573e64a99af8f702bdcc8242b74c67b26b261106a5c3dadfdd1174992face264201709c0'
